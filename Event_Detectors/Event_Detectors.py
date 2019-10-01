2e04d260e25be060af0aeaff5131b54525921fbd
Daniel Jorde <daniel.jorde@tum.de>
2019-10-01 17:29:06 +0200

    Altered __init__

--- Event_Detectors/Event_Detectors.py
+++ Event_Detectors/Event_Detectors.py
@@ -1,4 +1,3 @@
-import Utility_functions
 import platform
 import matplotlib
 if platform.system() == "Linux": #for matplotlib on Linux
@@ -303,7 +302,7 @@ class EventDet_MEED(BaseEstimator, ClassifierMixin):
         if len(current_p) == 0:
             raise ValueError("The current signal you have provided, is empty! Please check your input!")
 
-        Electrical_Metrics = Utility_functions.Electrical_Metrics()
+        Electrical_Metrics = Electrical_Metrics()
 
         current_rms = Electrical_Metrics.compute_single_rms(current_p, period_length=int(
             period_length_p * self.rms_periods))  # Preprocessing Function for the Stream
@@ -986,7 +985,7 @@ class EventDet_Jin(BaseEstimator, ClassifierMixin):
 
         """
         #compute the active power
-        Electrical_Metrics = Utility_functions.Electrical_Metrics()
+        Electrical_Metrics = Electrical_Metrics()
         active_power = Electrical_Metrics.active_power(voltage,current,period_length)
         return active_power
 
@@ -1375,7 +1374,7 @@ class EventDet_Zheng(BaseEstimator, ClassifierMixin):
 
         """
         # 1. Compute the Metrics
-        Metrics = Utility_functions.Electrical_Metrics()
+        Metrics = Electrical_Metrics()
         current_rms = Metrics.compute_single_rms(signal=current, period_length=period_length, )
         active_power = Metrics.active_power(instant_voltage=voltage, instant_current=current,
                                             period_length=period_length)
@@ -2413,7 +2412,7 @@ class EventDet_Barsim_Sequential(BaseEstimator, ClassifierMixin):
         self.original_non_log = original_non_log
 
         #Compute the active and reactive power using the Metrics class provided with this estimator
-        Metrics = Utility_functions.Electrical_Metrics()
+        Metrics = Electrical_Metrics()
 
         active_power_P= Metrics.active_power(instant_voltage=voltage, instant_current=current,
                                             period_length=compute_features_window) #values per second
@@ -2994,7 +2993,7 @@ class EventDet_Liu_Ripple(BaseEstimator, ClassifierMixin):
 
 
         # Compute the active and reactive power using the Metrics class provided with this estimator
-        Metrics = Utility_functions.Electrical_Metrics()
+        Metrics = Electrical_Metrics()
 
         active_power_P = Metrics.active_power(instant_voltage=voltage, instant_current=current,
                                               period_length=period_length)
@@ -3175,3 +3174,133 @@ class EventDet_Liu_Ripple(BaseEstimator, ClassifierMixin):
 
         return results
 
+
+class Electrical_Metrics:
+    def __init__(self):
+        pass
+
+    def active_power(self,instant_voltage, instant_current,period_length):
+        """
+        Active or Real power is the average of instantaneous power.
+        P = Sum ( i[n] * v[n] ) / N )
+        First we calculate the instantaneous power by multiplying the instantaneous
+        voltage measurement by the instantaneous current measurement. We sum the
+        instantaneous power measurement over a given number of samples and divide by
+        that number of samples.
+
+        Args:
+            instant_voltage: numpy array
+            instant_current: numpy array
+
+        Returns:
+            active power: numpy array
+        """
+        instant_current = np.array(instant_current).flatten()
+        instant_voltage = np.array(instant_voltage).flatten()
+
+        if len(instant_current) == len(instant_voltage):
+            instant_power = instant_voltage * instant_current
+            period_length = int(period_length)
+
+            active_power = []
+            for i in range(0, len(instant_power), period_length):
+                if i + period_length <= len(instant_power):
+                    signal_one_period = instant_power[i:int(i + period_length)]
+                    active_power_one_period = np.mean(signal_one_period )
+                    active_power.append(active_power_one_period)
+            active_power = np.array(active_power)
+            return active_power
+
+        else:
+            raise ValueError("Signals need to have the same length")
+
+
+    def apparent_power(self, instant_voltage,instant_current,period_length):
+        """
+        S = Vrms * Irms
+        Args:
+            instant_voltage: numpy array
+            instant_current: numpy array
+            period_length: numpy array
+
+        Returns:
+            apparent power: numpy array
+        """
+        if len(instant_current) == len(instant_voltage):
+
+            rms_voltage = self.compute_single_rms(instant_voltage,period_length)
+            rms_current = self.compute_single_rms(instant_current,period_length)
+            apparent_power = rms_voltage * rms_current
+            return apparent_power
+
+        else:
+            raise ValueError("Signals need to have the same length")
+
+
+    def reactive_power(self,apparent_power,active_power):
+        """
+
+        Q = sqrt(S^2 - P^2)
+        Args:
+            apparent_power: numpy array
+            active_power: numpy array
+
+        Returns:
+            reactive power: numpy array
+
+        """
+        if len(apparent_power) == len(active_power):
+            reactive_power = np.sqrt((apparent_power * apparent_power) - (active_power * active_power))
+            return reactive_power
+        else:
+            raise ValueError("Signals need to have the same length")
+
+    def compute_power_factor(self,apparent_power,active_power):
+        """
+        PF = P / S
+        Args:
+            apparent_power: numpy array
+            active_power: numpy array
+
+        Returns:
+            power factor: single integer
+        """
+
+        power_factor = active_power / apparent_power
+        return power_factor
+
+    def compute_voltage_current_rms(self, voltage, current, period_length):
+        """
+
+        Args:
+            voltage: numpy array
+            current: numpy array
+            period_length: integer
+
+        Returns:
+            voltage_rms: numpy array
+            current_rms: numpy array
+        """
+        period_length = int(period_length)
+        voltage_rms = self.compute_single_rms(voltage, period_length)
+        current_rms = self.compute_single_rms(current, period_length)
+        return voltage_rms, current_rms
+
+    def compute_single_rms(self,signal,period_length):
+        """
+
+        Args:
+            signal: numpy array
+            period_length: in samples: can be the net frequency or a multiple of it
+
+        Returns:
+            rms_values
+        """
+        rms_values = []
+        period_length = int(period_length)
+        for i in range(0, len(signal), period_length):
+            if i + period_length <= len(signal):
+                signal_one_period = signal[i:int(i + period_length)]
+                rms_one_period = np.sqrt(np.mean(np.square(signal_one_period))) #rms
+                rms_values.append(rms_one_period)
+        return np.array(rms_values)
--- Event_Detectors/__init__.py
+++ Event_Detectors/__init__.py
@@ -2,3 +2,4 @@ from Event_Detectors import EventDet_Liu_Ripple
 from Event_Detectors import EventDet_Barsim_Sequential
 from Event_Detectors import EventDet_Zheng
 from Event_Detectors import EventDet_Jin
+from Event_Detectors import EventDet_MEED
