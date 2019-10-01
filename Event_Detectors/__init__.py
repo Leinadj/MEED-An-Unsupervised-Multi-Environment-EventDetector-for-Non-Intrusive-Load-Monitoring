12b4a608f21575f5ebf11c5164edeffb82eb603a
Daniel Jorde <daniel.jorde@tum.de>
2019-10-01 17:12:44 +0200

    Create Requirements.txt and added Algorithms and Project Structure

---
+++ Event_Detectors/__init__.py
@@ -0,0 +1,4 @@
+from Event_Detectors import EventDet_Liu_Ripple
+from Event_Detectors import EventDet_Barsim_Sequential
+from Event_Detectors import EventDet_Zheng
+from Event_Detectors import EventDet_Jin
